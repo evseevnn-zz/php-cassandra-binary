c0fca23ae5e8b62eaf1a0b74bb70dfc333c7b3bd
Lars Fronius <l.fronius@googlemail.com>
2014-10-09 16:49:10 +0200

    basic testing setup on travis

---
+++ tests/Cassandra/Tests/ConnectionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace evseevnn\Cassandra\Tests;
+use evseevnn\Cassandra;
+
+class ConnectionTest extends \PHPUnit_Framework_TestCase {
+
+    public function testCreateConnection()
+    {
+        $connection = new Cassandra\Database(['127.0.0.1:9042']);
+        $connection->connect();
+    }
+
+    public function testNonFunctioningConnection()
+    {
+        $this->markTestSkipped('needs better connection handling');
+        $connection = new Cassandra\Database(['127.0.0.1:9201']);
+        $connection->connect();
+    }
+
+}
