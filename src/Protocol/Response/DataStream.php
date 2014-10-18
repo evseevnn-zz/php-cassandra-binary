d9af69d65fbc3dcded684f7c1d2acd3193cf249c
andreizet <zuica.andrei@gmail.com>
2014-10-18 16:48:09 +0300

    Fixed indentation;
    Fixed indentation;

--- src/Protocol/Response/DataStream.php
+++ src/Protocol/Response/DataStream.php
@@ -73,6 +73,68 @@ class DataStream {
     }
 
 	/**
+  	 * Read unsigned big int;
+  	 *
+  	 * @return int;
+  	 */
+ 	function readBigInt() {
+   		$data = $this->data;
+   		$arr = unpack('N2', $data);
+
+		if (PHP_INT_SIZE == 4) {
+    		$hi = $arr[1];
+	    	$lo = $arr[2];
+    		$isNeg = $hi  < 0;
+
+	    	// Check for a negative
+     		if ($isNeg) {
+       			$hi = ~$hi & (int)0xffffffff;
+       			$lo = ~$lo & (int)0xffffffff;
+
+	       		if ($lo == (int)0xffffffff) {	
+    	     		$hi++;
+        	 		$lo = 0;
+       			} else {
+         			$lo++;
+       			}
+     		}
+
+     		// Force 32bit words in excess of 2G to pe positive - we deal wigh sign
+     		// explicitly below
+     		if ($hi & (int)0x80000000) {
+       			$hi &= (int)0x7fffffff;
+       			$hi += 0x80000000;
+     		}
+
+    		if ($lo & (int)0x80000000) {
+      			$lo &= (int)0x7fffffff;
+      			$lo += 0x80000000;
+    		}
+
+     		$value = $hi * 4294967296 + $lo;
+
+     		if ($isNeg) {
+       			$value = 0 - $value;
+     		}
+   		} else {
+     		if ($arr[2] & 0x80000000) {
+       			$arr[2] = $arr[2] & 0xffffffff;
+     		}
+
+     		if ($arr[1] & 0x80000000) {
+       			$arr[1] = $arr[1] & 0xffffffff;
+       			$arr[1] = $arr[1] ^ 0xffffffff;
+       			$arr[2] = $arr[2] ^ 0xffffffff;
+       			$value = 0 - $arr[1]*4294967296 - $arr[2] - 1;
+     		} else {
+       			$value = $arr[1]*4294967296 + $arr[2];
+     		}
+   		}
+
+		return $value;
+ 	}
+
+	/**
 	 * Read string.
 	 *
 	 * @return string
@@ -224,72 +286,6 @@ class DataStream {
 		return substr($value, 0, $len - $scale) . '.' . substr($value, $len - $scale);
 	}
 
-  /**
-   * Read signed big int (64 bits)
-   *
-   * @return bigint
-   */
-  function readBigInt() {
-    $data = $this->data;
-    $arr = unpack('N2', $data);
-
-    // If we are on a 32bit architecture we have to explicitly deal with
-    // 64-bit twos-complement arithmetic since PHP wants to treat all ints
-    // as signed and any int over 2^31 - 1 as a float
-    if (PHP_INT_SIZE == 4) {
-
-      $hi = $arr[1];
-      $lo = $arr[2];
-      $isNeg = $hi  < 0;
-
-      // Check for a negative
-      if ($isNeg) {
-        $hi = ~$hi & (int)0xffffffff;
-        $lo = ~$lo & (int)0xffffffff;
-
-        if ($lo == (int)0xffffffff) {
-          $hi++;
-          $lo = 0;
-        } else {
-          $lo++;
-        }
-      }
-
-      if ($hi & (int)0x80000000) {
-        $hi &= (int)0x7fffffff;
-        $hi += 0x80000000;
-      }
-
-      if ($lo & (int)0x80000000) {
-        $lo &= (int)0x7fffffff;
-        $lo += 0x80000000;
-      }
-
-      $value = $hi * 4294967296 + $lo;
-
-      if ($isNeg) {
-        $value = 0 - $value;
-      }
-    } else {
-
-      if ($arr[2] & 0x80000000) {
-        $arr[2] = $arr[2] & 0xffffffff;
-      }
-
-      // Check for a negative
-      if ($arr[1] & 0x80000000) {
-        $arr[1] = $arr[1] & 0xffffffff;
-        $arr[1] = $arr[1] ^ 0xffffffff;
-        $arr[2] = $arr[2] ^ 0xffffffff;
-        $value = 0 - $arr[1]*4294967296 - $arr[2] - 1;
-      } else {
-        $value = $arr[1]*4294967296 + $arr[2];
-      }
-    }
-
-    return $value;
-  }
-
 	/**
 	 * @param array $type
 	 * @param bool $isCollectionElement for collection element used other alg. a temporary solution
@@ -302,7 +298,7 @@ class DataStream {
 			case DataTypeEnum::TEXT:
 				return $isCollectionElement ? $this->readString() : $this->data;
 			case DataTypeEnum::BIGINT:
-        return $this->readBigInt();
+        		return $this->readBigInt();
 			case DataTypeEnum::COUNTER:
 			case DataTypeEnum::VARINT:
 				return $this->readVarint();
