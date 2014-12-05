a046894b3ff06f4e7e82545c08b20a434c8f8ffa
glasek <glasek@red-sky.pl>
2014-12-05 11:45:55 +0100

    Indendation

--- src/Protocol/BinaryData.php
+++ src/Protocol/BinaryData.php
@@ -236,14 +236,14 @@ class BinaryData {
 	 * @param string $dec
 	 * @return string
 	 */
-	private function bcdechex($dec) {
+    private function bcdechex($dec) {
         $last = bcmod($dec, 16);
-		$remain = bcdiv(bcsub($dec, $last), 16);
+        $remain = bcdiv(bcsub($dec, $last), 16);
 
-		if ($remain == 0)
-			return dechex($last);
+        if ($remain == 0)
+            return dechex($last);
 
-		return $this->bcdechex($remain) . dechex($last);
+        return $this->bcdechex($remain) . dechex($last);
 	}
 
 }
